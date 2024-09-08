77da33c9701287c7edc77eaa157f67915d96c6c9
mfsantarena <florsantarena@gmail.com>
2024-09-08 11:50:10 -0300

    aritmetica 1

---
+++ Evidencia_1/Programacion/aritmetica.py
@@ -0,0 +1,33 @@
+
+
+def sumar(a,b):
+    try:
+        c = a + b
+        return c
+    except Exception as e:
+        print("Dato ingresado no valido")
+ 
+
+def restar(a,b):
+    try:
+        c = a - b
+        return c
+    except Exception as e:
+        print("Dato ingresado no valido")
+
+def dividir(a,b):
+    try: 
+        c = a/b
+        return c
+    except ZeroDivisionError as e:
+        print("La division por 0 no esta definida")
+    except ValueError as e:
+        print("No ingresaste un valor valido")
+
+def multiplicar(a,b):
+    try:
+        c = a * b
+        return c
+    except Exception as e:
+        print("Dato ingresado no valido")
+
