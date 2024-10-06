afd88eca9afe2a59e97c0391ff20b6aa0621fe1f
cjuangab <cabrerajuangab@gmail.com>
2024-10-06 15:35:02 -0300

    programacion evidencia 2

--- Evidencia_2/users.py
+++ Evidencia_2/users.py
@@ -1,15 +1,20 @@
-class Usuario:
+import pickle           #Importamos la libreria pickle que nos permite convertir objetos a binarios 
+
+class Usuario:          #Definicion de la clase Usuario
+#Definicion de variables privadas
     __id:int
     __username:str
     __password:str
     __email:str
 
-    def __init__(self,id:int, username:str, password:str, email:str) -> None:
+
+    def __init__(self,id:int, username:str, password:str, email:str) -> None:   #Constructur
         self.__id = id
         self.__username = username
         self.__password = password
         self.__email = email
-     
+
+#Geters    
     def getId(self):
         return self.__id
     def getUsername(self):
@@ -18,22 +23,31 @@ class Usuario:
         return self.__password
     def getEmail(self):
         return self.__email
-    
+#Seters    
+    def setUsername(self, username:str):
+        self.__username=username
+    def setPassword(self, password:str):
+        self.__password=password
+    def setEmail(self, email:str):
+        self.__email=email
 
+    def __str__(self) -> str:
+        return f"ID: {self.__id} Nombre de Usuario: {self.__username}, Email: {self.__email}"
 
-
-class Acceso:
+class Acceso:               #Definicion de clase Acceso
+#Definicion de variables privadas
     __id :int
     __fechaIngreso:str
     __fechaSalida:str
     __usuarioLogueado:str
 
-    def __init__(self, id:int, fechaIngreso:str, fechaSalida:str, usuarioLogueado:str) -> None:
+    def __init__(self, id:int, fechaIngreso:str, fechaSalida:str, usuarioLogueado:str) -> None: #Constructor
         self.__id = id
         self.__fechaIngreso = fechaIngreso
         self.__fechaSalida = fechaSalida
         self.__usuarioLogueado = usuarioLogueado
 
+#Geters
     def getId(self):
         return self.__id
     def getFechaIngreso(self):
@@ -42,9 +56,86 @@ class Acceso:
         return self.__fechaSalida
     def getUsuarioLogueado(self):
         return self.__usuarioLogueado
+#Seters
+    def setFechaIngreso(self,fecha_ingreso:str):
+        self.__fechaIngreso = fecha_ingreso
+    def setFechaSalida(self, fecha_salida:str):
+        self.__fechaSalida = fecha_salida
+    def setUsuarioLogeado(self, usuario_logeado:str):
+        self.__usuarioLogueado = usuario_logeado
+
+    def __str__(self) -> str:
+        return f" ID: {self.__id} - Usuario: {self.__usuarioLogueado} - Fecha de Ingreso: {self.__fechaIngreso} - Fecha de Egreso: {self.__fechaSalida}"
+
+def leer_usuarios(archivo = "usuarios.ispc"):   #Definimos una funcion para leer "usuarios" de un archivo binario, por defecto usuarios.ispc
+    with open(archivo,"rb") as archivo:         #Abrimos el documento guardado en archivo y con "rb" definimos que va a ser lectura de un archivo binario
+        usuario = pickle.load(archivo)          # Almacenamos en la variable usuario la informacion leida a traves del metodo pickle.load que permite la interpretacion binaria
+    return usuario
+
+def agregar_usuario(usuario:Usuario, archivo = "usuarios.ispc"): #Definimos una funcion para agregar "usuarios" de un archivo binario, por defecto usuarios.ispc
+    try:
+        usuarios_en_lista = leer_usuarios(archivo)              #Utilizamos la funcion predefinina que internamente nos devuelve una lista
+    except FileNotFoundError:
+        usuarios_en_lista = []
+    """
+    El try/except es para manejar alguan error que pueda aparecer por no existir el archivo o estar en blanco
+    """
+    usuarios_en_lista.append(usuario)                           #Agregamos a el usuario a la lista existente en el archivo 
+
+    with open(archivo, 'wb') as archivo:                        
+        pickle.dump(usuarios_en_lista, archivo)                 #Sobreescribimos la lista existente con la nueva lista que contiene al usuario nuevo
     
+    print(f"El Usuario: {usuario.getUsername()} ha sido agregado exitosamente")
 
-usuario1= Usuario(1323,"cjuangab","qwerty","tumama@tumama")
+    """
+    En esta funcion utilizamos el metodo wb que sobre escribe la informacion que haya en el archivo binario
+    utilizamos este metodo porque al trabajar con poca informacion va a ser mas eficiente y mas facil de
+    trabajar
+    """
 
-print(usuario1.getUsername())
-print(usuario1.getEmail())
+def mostrar_usuarios(archivo='usuarios.ispc'):  #Definimos una funcion para Mostrar "usuarios" de un archivo binario, por defecto usuarios.ispc
+    try:
+        usuarios_lista = leer_usuarios(archivo)
+        for usuario in usuarios_lista:
+            print(usuario)
+    except FileNotFoundError:
+        print("No se encontraron usuarios.")
+    """
+    Con el try/except trabajamos para garantizar que el programa no falle si no llega a existir el archivo
+    o llega a estar vacio
+    """
+    
+def actualizar_usuario(username:str, password:str, email:str, archivo="usuarios.ispc"):
+    usuarios_lista = leer_usuarios(archivo)
+    
+    for usuario in usuarios_lista:
+        if usuario.getUsername == username:
+            usuario.setUsername(username)
+            usuario.setPassword(password)
+            usuario.setNmail(email)
+
+            # Guardar los cambios en el archivo binario
+            with open(archivo, 'wb') as archivo:
+                pickle.dump(usuarios_lista, archivo)
+            print(f"Usuario con Datos actualizados {usuario.__str__()} .")
+            return
+    print(f"Usuario con ID {id} no encontrado.")
+
+def buscar_usuario(id_usuario, archivo='usuarios.ispc'):
+
+    usuarios_lista = leer_usuarios(archivo)
+    for usuario in usuarios_lista:
+        if usuario.getId() == id_usuario:
+            return usuario
+    return None
+
+def eliminar_usuario(id, archivo='usuarios.ispc'):     #Definimos una funcion para Eliminar "usuarios" de un archivo binario, por defecto usuarios.ispc
+    usuarios_lista = leer_usuarios(archivo)
+    
+    usuarios_filtrados = [usuario for usuario in usuarios_lista if usuario.get_id() != id]
+
+    # Guardar los cambios en el archivo binario
+    with open(archivo, 'wb') as archivo:
+        pickle.dump(usuarios_filtrados, archivo)
+    
+    print(f"Usuario con ID {id} eliminado.")
